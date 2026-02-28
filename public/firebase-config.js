4d9dbabe7887c360b34a17d2a24d8499bf81ee22
Belal-90 <hossainbelal20790@gmail.com>
2026-02-28 23:41:06 +0300

    Set runtime Firebase web config values

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -1,10 +1,10 @@
 // Optional runtime Firebase config for hosts where env vars are not injected at build time.
 // Fill values below and redeploy if your production environment cannot provide VITE_FIREBASE_* vars.
 window.__FIREBASE_CONFIG__ = {
-  apiKey: '',
-  authDomain: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: '',
+  apiKey: 'AIzaSyDP6K7-DSOB6CJbI-ME6EAEfWfdQ3VTrpw',
+  authDomain: 'kz-ggl-istd-frbs-gt-usr-b20790.firebaseapp.com',
+  projectId: 'kz-ggl-istd-frbs-gt-usr-b20790',
+  storageBucket: 'kz-ggl-istd-frbs-gt-usr-b20790.firebasestorage.app',
+  messagingSenderId: '40179743590',
+  appId: '1:40179743590:web:2746d623d8063c44d326a7',
 }
